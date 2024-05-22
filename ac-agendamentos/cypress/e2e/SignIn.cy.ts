01b679bfd8b4ed2f0932d65c14c240a9e3bcce2c
Alex <alexsander.ventricci@gmail.com>
2024-05-22 17:32:27 -0300

    feat adiciona teste integração de falha

--- ac-agendamentos/cypress/e2e/SignIn.cy.ts
+++ ac-agendamentos/cypress/e2e/SignIn.cy.ts
@@ -6,4 +6,13 @@ describe("SignIn", () => {
     cy.get("button[id=submit").click();
     cy.url().should("include", "/dashboard");
   });
+  it("should not signIn and open error alert", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("input[id=email]").type("sadfasdf.fasdf@gmail.com");
+    cy.get("input[id=password]").type("asdfasdf");
+    cy.get("button[id=submit").click();
+    cy.contains(
+      "Houve um erro ao tentar se conectar, verifique os dados inseridos e tente novamente."
+    );
+  });
 });
